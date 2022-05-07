b80de1c337eab50c7191e02e03fbb4d315466b55
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2022-05-07 17:24:47 +0700

    fix bug

---
+++ dist/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var jsx_runtime_1 = require("react/jsx-runtime");
+function index() {
+    return jsx_runtime_1.jsx("button", { children: "React Input Suggestion" }, void 0);
+}
+exports.default = index;
